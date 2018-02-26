ffacfab8f11d75980fe2679583cb38c248beb445
Stephanie Liu <sliu14@outlook.com>
2018-02-25 22:47:00 -0500

    new logo and contextual inquiry check in

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -18,4 +18,9 @@ var store = [{
         "excerpt":"Our design aims to promote a more inclusive community centered around public art. Our target users are those who lack the formal background knowledge to more deeply appreciate and learn about art pieces seen in museums. Providing a resource that allows people to learn more about and discuss art also...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/hciproject/contextual-inquiry-plan/",
+        "teaser":null},{
+        "title": "Contextual Inquiry Check-In",
+        "excerpt":"SetupOur entire team went to MASS MoCA on Saturday and each interviewed a different demographic for about 30 minutes to an hour each.InterviewsGroup A:Demographic: Two students, aged 19 and 21, at MCLA and Worcester State University.Background: Both students had some experience with art, with one being only tangentially interested in...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/hciproject/contextual-inquiry-check-in/",
         "teaser":null}]
